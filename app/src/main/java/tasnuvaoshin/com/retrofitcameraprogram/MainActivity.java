6e9c4a1deee9cd18f2cd94b626cd7cd9c553f865
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2020-01-17 22:37:38 +0600

    Image Upload Using Volley

---
+++ app/src/main/java/tasnuvaoshin/com/retrofitcameraprogram/MainActivity.java
@@ -0,0 +1,223 @@
+package tasnuvaoshin.com.retrofitcameraprogram;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private Button camera, gallary;
+    private ImageView imageView;
+    private String upload_URL = "http://joy-technologies-ltd.com/test/upload.php";
+    JSONObject jsonObject;
+    RequestQueue rQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.bt_camera);
+        gallary = findViewById(R.id.bt_gallary);
+        imageView = findViewById(R.id.image_view);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (CheckPermission()) {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+            }
+        });
+
+        gallary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (CheckPermission()) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);
+                }
+            }
+        });
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, final Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+
+                    Bitmap bitmap = (Bitmap) imageReturnedIntent.getExtras().get("data");
+                    imageView.setImageBitmap(bitmap);
+                    uploadImage(bitmap);
+                }
+
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+
+
+                    try {
+                        Uri selectedImage = imageReturnedIntent.getData();
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                        uploadImage(bitmap);
+                        Toast.makeText(this, "Ready to Upload", Toast.LENGTH_SHORT).show();
+                        imageView.setImageURI(selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //      uploadImage(bitmap);
+                }
+                break;
+        }
+    }
+
+    //through volley 
+    private void uploadImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+        try {
+            jsonObject = new JSONObject();
+            String imgname = String.valueOf(Calendar.getInstance().getTimeInMillis());
+            Log.d("name", imgname);
+            jsonObject.put("name", imgname);
+            //  Log.e("Image name", etxtUpload.getText().toString().trim());
+            jsonObject.put("image", encodedImage);
+            // jsonObject.put("aa", "aa");
+        } catch (JSONException e) {
+            Log.e("JSONObject Here", e.toString());
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, upload_URL, jsonObject,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Log.e("aaaaaaa", jsonObject.toString());
+                        rQueue.getCache().clear();
+                        Toast.makeText(getApplication(), "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("aaaaaaa", volleyError.toString());
+
+            }
+        });
+
+        rQueue = Volley.newRequestQueue(MainActivity.this);
+        rQueue.add(jsonObjectRequest);
+
+    }
+
+
+    public boolean CheckPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Permission")
+                        .setMessage("Please Share/on Your Location")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+                                startActivity(new Intent(MainActivity
+                                        .this, MainActivity.class));
+                                MainActivity.this.overridePendingTransition(0, 0);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+
+            return false;
+        } else {
+
+            return true;
+
+        }
+    }
+
+
+    private Bitmap uriToBitmap(Uri selectedFileUri) {
+        try {
+            ParcelFileDescriptor parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            parcelFileDescriptor.close();
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
